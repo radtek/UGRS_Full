ff56ae3271c7f7308bd2c4deff7ec3fd9ee49b47
AlbertoMtz <albertomtz12@gmail.com>
2019-07-23 16:22:08 -0700

    Carga de matrix

--- UGRS.2018/UGRS.AddOn.CreditNote/Forms/frmCreditNote.b1f.cs
+++ UGRS.2018/UGRS.AddOn.CreditNote/Forms/frmCreditNote.b1f.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SAPbouiCOM.Framework;
+using UGRS.Core.Services;
+using UGRS.Core.SDK.UI;
+using UGRS.Core.SDK.DI.CreditNote;
 
 namespace UGRS.AddOn.CreditNote.Forms
 {
     [FormAttribute("UGRS.AddOn.CreditNote.Forms.frmCreditNote", "Forms/frmCreditNote.b1f")]
     class frmCreditNote : UserFormBase
     {
+        CreditNoteFactory mObjCreditNoteFactory = new CreditNoteFactory();
         public frmCreditNote()
         {
         }
@@ -39,6 +43,55 @@ namespace UGRS.AddOn.CreditNote.Forms
 
         }
 
+        private void SearchValue()
+        {
+            try
+            {
+                this.UIAPIRawForm.Freeze(true);
+                SetDataTableValues();
+                BindMatrix();
+            }
+            catch (Exception ex)
+            {
+                this.UIAPIRawForm.Freeze(false);
+                LogService.WriteError(ex.Message);
+                LogService.WriteError(ex);
+                UIApplication.ShowMessageBox(ex.Message);
+            }
+            finally
+            {
+                this.UIAPIRawForm.Freeze(false);
+                //mObjProgressBar.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Carga los pagos desde una consulta al datatable
+        /// </summary>
+        private void SetDataTableValues()
+        {
+            DtMatrix = this.UIAPIRawForm.DataSources.DataTables.Item("Dt_INV");
+            DateTime lDtmDate = Convert.ToDateTime(txtDate.Value);
+           
+            DtMatrix.ExecuteQuery(mObjCreditNoteFactory.GetCreditNoteService().GetInvoiceQuery(lDtmDate));
+        }
+
+        /// <summary>
+        /// Asigna las columnas del datatable a la matriz
+        /// </summary>
+        private void BindMatrix()
+        {
+            foreach (SAPbouiCOM.Column item in mtxInv.Columns)
+            {
+                mtxInv.Columns.Item(item.UniqueID).DataBind.Bind("Dt_INV", item.UniqueID);
+            }
+            mtxInv.LoadFromDataSource();
+            mtxInv.AutoResizeColumns();
+           
+        }
+
+
+        private SAPbouiCOM.DataTable DtMatrix;
         private SAPbouiCOM.EditText txtDate;
         private SAPbouiCOM.Button btnNC;
         private SAPbouiCOM.Button btnReport;
