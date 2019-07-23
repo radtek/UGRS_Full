667b97d4821f891c99c27ac798b0c0b89b2cacb9
AlbertoMtz <albertomtz12@gmail.com>
2019-07-23 12:00:34 -0700

    Inicio de proyecto Addon.CreditNote e Interfaz

---
+++ UGRS.2018/UGRS.AddOn.CreditNote/Forms/frmCreditNote.b1f.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace UGRS.AddOn.CreditNote.Forms
+{
+    [FormAttribute("UGRS.AddOn.CreditNote.Forms.frmCreditNote", "Forms/frmCreditNote.b1f")]
+    class frmCreditNote : UserFormBase
+    {
+        public frmCreditNote()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_1").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("btnNC").Specific));
+            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("btnReport").Specific));
+            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("mtxInv").Specific));
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_5").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.Button Button0;
+        private SAPbouiCOM.Button Button1;
+        private SAPbouiCOM.Matrix Matrix0;
+        private SAPbouiCOM.StaticText StaticText0;
+    }
+}
---
+++ UGRS.2018/UGRS.AddOn.CreditNote/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SAPbouiCOM.Framework;
+using UGRS.AddOn.CreditNote.Forms;
+
+namespace UGRS.AddOn.CreditNote
+{
+    class Menu
+    {
+        public void AddMenuItems()
+        {
+            SAPbouiCOM.Menus oMenus = null;
+            SAPbouiCOM.MenuItem oMenuItem = null;
+
+            oMenus = Application.SBO_Application.Menus;
+
+            SAPbouiCOM.MenuCreationParams oCreationPackage = null;
+            oCreationPackage = ((SAPbouiCOM.MenuCreationParams)(Application.SBO_Application.CreateObject(SAPbouiCOM.BoCreatableObjectType.cot_MenuCreationParams)));
+            oMenuItem = Application.SBO_Application.Menus.Item("43520"); // moudles'
+
+            oCreationPackage.Type = SAPbouiCOM.BoMenuType.mt_POPUP;
+            oCreationPackage.UniqueID = "UGRS.AddOn.CreditNote";
+            oCreationPackage.String = "UGRS.AddOn.CreditNote";
+            oCreationPackage.Enabled = true;
+            oCreationPackage.Position = -1;
+
+            oMenus = oMenuItem.SubMenus;
+
+            try
+            {
+                //  If the manu already exists this code will fail
+                oMenus.AddEx(oCreationPackage);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            try
+            {
+                // Get the menu collection of the newly added pop-up item
+                oMenuItem = Application.SBO_Application.Menus.Item("UGRS.AddOn.CreditNote");
+                oMenus = oMenuItem.SubMenus;
+
+                // Create s sub menu
+                oCreationPackage.Type = SAPbouiCOM.BoMenuType.mt_STRING;
+                oCreationPackage.UniqueID = "UGRS.AddOn.CreditNote.Form1";
+                oCreationPackage.String = "Form1";
+                oMenus.AddEx(oCreationPackage);
+            }
+            catch (Exception er)
+            { //  Menu already exists
+                Application.SBO_Application.SetStatusBarMessage("Menu Already Exists", SAPbouiCOM.BoMessageTime.bmt_Short, true);
+            }
+        }
+
+        public void SBO_Application_MenuEvent(ref SAPbouiCOM.MenuEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+
+            try
+            {
+                if (pVal.BeforeAction && pVal.MenuUID == "UGRS.AddOn.CreditNote.Form1")
+                {
+                    frmCreditNote activeForm = new frmCreditNote();
+                    activeForm.Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Application.SBO_Application.MessageBox(ex.ToString(), 1, "Ok", "", "");
+            }
+        }
+
+    }
+}
